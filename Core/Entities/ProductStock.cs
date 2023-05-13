997c44ae3b5963c12f73ef5f6179e32887bef817
Marina Zelenskaya <60691459+zelensskaya@users.noreply.github.com>
2023-05-13 13:17:26 +0200

    API Architecture

---
+++ Core/Entities/ProductStock.cs
@@ -0,0 +1,7 @@
+namespace Core.Entities
+{
+    public class ProductStock : BaseEntity
+    {
+        public string Name { get; set; }
+    }
+}
---
+++ Infrastructure/Data/StoreContextSeed.cs
@@ -0,0 +1,42 @@
+using System.Text.Json;
+using Core.Entities;
+using Infrastructure.Data;
+
+namespace Infrastructure.Data
+{
+    public class StoreContextSeed
+    {
+        public static async Task SeedAsync(StoreContext context)
+        {
+            if (!context.ProductBrands.Any())
+            {
+                var brandsData = File.ReadAllText("..\\Infrastructure\\Data\\SeedData\\brands.json");
+                var brands = JsonSerializer.Deserialize<List<ProductBrand>>(brandsData);
+                context.ProductBrands.AddRange(brands);
+            }
+
+            if (!context.ProductTypes.Any())
+            {
+                var typesData = File.ReadAllText("..\\Infrastructure\\Data\\SeedData\\types.json");
+                var types = JsonSerializer.Deserialize<List<ProductType>>(typesData);
+                context.ProductTypes.AddRange(types);
+            }
+
+            if (!context.ProductStocks.Any())
+            {
+                var stocksData = File.ReadAllText("../Infrastructure/Data/SeedData/stocks.json");
+                var stocks = JsonSerializer.Deserialize<List<ProductStock>>(stocksData);
+                context.ProductStocks.AddRange(stocks);
+            }
+
+            if (!context.Products.Any())
+            {
+                var productsData = File.ReadAllText("../Infrastructure/Data/SeedData/products.json");
+                var products = JsonSerializer.Deserialize<List<Product>>(productsData);
+                context.Products.AddRange(products);
+            }
+
+            if (context.ChangeTracker.HasChanges()) await context.SaveChangesAsync();
+        }
+    }
+}
